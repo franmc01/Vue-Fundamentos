4c695e24b4682ffa46ba42734fb2781ec5a063e6
franciscomarin98 <francmarinc@gmail.com>
2021-06-19 10:29:51 -0500

    Introduccion a los componentes

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+app.component('footer-banco', {
+    template:/*html*/`
+    <div class="bg-dark py-3 mt-2 text-sm text-white text-center">
+        <p>Footer de mi sitio web</p>
+    </div>
+    `
+});
