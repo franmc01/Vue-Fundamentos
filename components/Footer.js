2dba608c2632ae4a2a44e3d64876d344a6b80f31
franciscomarin98 <francmarinc@gmail.com>
2021-06-19 10:53:28 -0500

    Usando los props

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,19 @@
 app.component('footer-banco', {
+    name: 'Footer',
+    props: {
+        anio: {
+            type: Number,
+            default: 2022
+        }
+    },
+    data() {
+        return {
+            texto: 'Francisco Marin - Todos los derechos reservados'
+        }
+    },
     template:/*html*/`
     <div class="bg-dark py-3 mt-2 text-sm text-white text-center">
-        <p>Footer de mi sitio web</p>
+        <p> {{ anio }} {{ texto }}</p>
     </div>
-    `
+    `,
 });
